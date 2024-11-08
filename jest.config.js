649807bf2bf0aedea3dffffadd48d2200f8cd0af
Ajit Yadav <mfbb1528@gmail.com>
2024-11-08 19:19:33 +0530

    RAM COMMIT 4

--- jest.config.js
+++ jest.config.js
@@ -11,6 +11,21 @@ const customJestConfig = {
     "^@/(.*)$": "<rootDir>/src/$1",
   },
   testEnvironment: "jest-environment-jsdom",
+  
+  // Enable coverage collection
+  collectCoverage: true,
+  
+  // Specify the directory where coverage files will be saved
+  coverageDirectory: "<rootDir>/coverage",  // Set coverage folder relative to project root
+  
+  // Choose which files to collect coverage for
+  collectCoverageFrom: [
+    "src/**/*.{js,ts,jsx,tsx}",  // Collect coverage from all files in src folder
+    "!src/**/*.d.ts",  // Exclude TypeScript declaration files
+  ],
+
+  // Specify the coverage reporters
+  coverageReporters: ["text", "lcov", "clover"],  // You can adjust these based on your needs
 };
 
 module.exports = createJestConfig(customJestConfig);
